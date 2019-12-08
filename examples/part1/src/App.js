f55d3eab07d89413e2db757797cc4d593f7b57d0
Izabella Oliveira <izabellapoliv@gmail.com>
2019-12-07 21:57:25 -0300

    update part1 example

--- examples/part1/src/App.js
+++ examples/part1/src/App.js
@@ -1,56 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './index.css';
 import logo from './logo.svg';
 import './App.css';
 
-const Footer = () => {
-  const now = new Date()
-  return (
-    <footer>
-      <div>
-        <small>copy @ {now.getFullYear()}</small>
-      </div>
-    </footer>
-  )
+const App = () => {
+    const [curTime, refreshTime] = useState(new Date())
+
+    const setToDate = (newDate) => () => refreshTime(newDate)
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>{curTime.toLocaleString()}</p>
+                <button onClick={setToDate(new Date())}
+                    className="App-link">
+                    Refresh time
+                </button>
+                <br />
+                <button onClick={setToDate(new Date('2019-02-18 02:40:37'))}
+                    className="App-link">
+                    Change date
+                </button>
+            </header>
+        </div>
+    );
 }
 
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hellooooo {props.name}, you are {props.age} years old.
-      </p>
-    </div>
-  )
-}
-
-function App() {
-  const now = new Date()
-  const num1 = 10
-  const num2 = 20
-  const person = { name: 'Nayara', age: 8 }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          It is now {now.toDateString()}
-        </p>
-        <Hello name="Julia" age={num1} />
-        <Hello name={person.name} age={person.age} />
-        <p>{num1} + {num2} = {num1 + num2}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Footer />
-    </div>
-  );
-}
-
-export default App;
+export default App
