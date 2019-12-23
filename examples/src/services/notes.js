33a2a23a2c4c8e917fba11415e75aaff188ec336
Izabella Oliveira <izabellapoliv@gmail.com>
2019-12-22 22:16:25 -0300

    finished part3b

--- examples/src/services/notes.js
+++ examples/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = '/api/notes'
 
 const getRequestObject = request => request.then(response => response.data)
 
